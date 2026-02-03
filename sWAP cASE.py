5752c15672840fccef307c159f1ca4285c5093ad
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-03 14:40:28 +0300

    Update sWAP cASE.py

--- sWAP cASE.py
+++ sWAP cASE.py
@@ -1,25 +1,13 @@
 def swap_case(s):
-
-answer = ""
-
-for letter in s:
-
-if letter.isupper():
-
-answer += letter.lower()
-
-else:
-
-answer += letter.upper()
-
-return answer
-
-  
+    answer = ""
+    for letter in s:
+        if letter.isupper():
+            answer += letter.lower()
+        else:
+            answer += letter.upper()
+    return answer
 
 if __name__ == '__main__':
-
-s = input()
-
-result = swap_case(s)
-
-print(result)
+    s = input()
+    result = swap_case(s)
+    print(result)
