36e6bc037ed0f9fe21b6dcff042b338784fed494
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-03 14:41:12 +0300

    Fix indentation and formatting in split_and_join function

--- String Split and Join.py
+++ String Split and Join.py
@@ -1,15 +1,8 @@
 def split_and_join(line):
-
-line = line.split(" ")
-
-line = "-".join(line)
-
-return line
-
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
 if __name__ == '__main__':
-
-line = input()
-
-result = split_and_join(line)
-
-print(result)
+    line = input()
+    result = split_and_join(line)
+    print(result)
