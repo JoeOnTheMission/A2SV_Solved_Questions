6f40947106cfc88bbcfaeb1e78fa9aa048650e1b
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-03 14:32:14 +0300

    Create sWAP cASE.py

---
+++ sWAP cASE.py
@@ -0,0 +1,25 @@
+def swap_case(s):
+
+answer = ""
+
+for letter in s:
+
+if letter.isupper():
+
+answer += letter.lower()
+
+else:
+
+answer += letter.upper()
+
+return answer
+
+  
+
+if __name__ == '__main__':
+
+s = input()
+
+result = swap_case(s)
+
+print(result)
