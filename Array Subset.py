4cd86ebe3ee9af390e2db587a618ded4eb03b563
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-04 15:31:37 +0300

    Create Array Subset.py

---
+++ Array Subset.py
@@ -0,0 +1,19 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        a.sort()
+        b.sort()
+        i = 0 
+        j = 0 
+        while i < len(a):
+            if a[i] == b[j]:
+                j += 1
+                if j == len(b):
+                    return True
+            i += 1
+        if j == len(b) :
+            return True
+        else:
+            return False
