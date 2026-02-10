902531991808047e9c75004f72664633528d1022
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-10 10:55:32 +0300

    Time: 19 ms (5.16%) | Memory: 23.5 MB (24.37%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-    
-        d = Counter(nums)
-        n = len(nums)
-        res =[]
-        for num in d:
-            if d[num] > n//3:
-                res.append(num)
-        return res
+        counter = defaultdict(int)
+        res = set()
+        for num in nums:
+            counter[num] += 1
+            if num not in res and counter[num] > len(nums)//3:
+                res.add(num)
+        return list(res)
