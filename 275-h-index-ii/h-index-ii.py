a03515b4f47105ae2f578a626239c300ca9e51cb
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-20 22:30:53 +0300

    Update h-index-ii.py

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        #this took 7 min
         n = len(citations)
         for i,val in enumerate(citations):
             if n - i <= val:
