c986a32effd8c10320e9e9456f7cb3b1eca0df34
JoeOnTheMission <joeonthemission@gmail.com>
2026-02-23 17:31:53 +0300

    Time: 3 ms (82.75%) | Memory: 20.6 MB (21.02%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,14 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        done = False
         holder = 0
-        while not done:
 
-            for seeker in range(holder,len(nums)):
-                if nums[seeker] != 0:
-                    nums[seeker],nums[holder] = nums[holder], nums[seeker]
-                    holder += 1
-                    continue
-            else:
-                done = True
+        for seeker in range(holder,len(nums)):
+            if nums[seeker] != 0:
+                nums[seeker],nums[holder] = nums[holder], nums[seeker]
+                holder += 1
+                continue
