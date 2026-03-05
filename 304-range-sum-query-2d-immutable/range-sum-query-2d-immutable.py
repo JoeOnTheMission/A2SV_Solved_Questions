676715fcb0421ca1c36eaf0d79f2ce2eb09aa0c3
JoeOnTheMission <joeonthemission@gmail.com>
2026-03-05 10:18:33 +0300

    Time: 104 ms (79.28%) | Memory: 33.7 MB (49.80%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -14,4 +14,9 @@ class NumMatrix:
        
         self.sub_sum = self.pre[row2][col2] - self.pre[row2][col1 - 1] - self.pre[row1 - 1][col2 ] + self.pre[row1 - 1][col1 - 1]
         return self.sub_sum
-
+"""        
+# Your NumMatrix object will be instantiated and called as such:
+matrix  = [[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]
+obj = NumMatrix(matrix)
+param_1 = obj.sumRegion(2,2,4,4)
+print(param_1)"""
