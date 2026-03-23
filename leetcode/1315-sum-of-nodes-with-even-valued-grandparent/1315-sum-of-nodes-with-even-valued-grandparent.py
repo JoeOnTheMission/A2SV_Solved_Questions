9f6979a393996f1b126a39bfe9df63d8502d20a8
JoeOnTheMission <joeonthemission@gmail.com>
2026-03-23 19:27:48 +0300

    Add solution for Sum of Nodes with Even-Valued Grandparent

--- leetcode/1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ leetcode/1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -5,25 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
-        stack = [root]
-        res = 0
-        while stack:
-            node = stack.pop()
+    def finder(self,cur,parent = None, grand = None):
+        if not cur:
+            return 
+        
+        if grand and grand.val % 2 == 0:
+            self.res += cur.val
+        self.finder(cur.left,cur,parent)
+        self.finder(cur.right,cur,parent)
 
-            if node and node.val % 2 ==0:
-                if node.left:
-                    if node.left.left:
-                        res += node.left.left.val
-                    if node.left.right:
-                        res += node.left.right.val
-                if node.right:
-                    if node.right.left:
-                        res += node.right.left.val
-                    if node.right.right:
-                        res += node.right.right.val
-            if node and node.left:
-                stack.append(node.left)
-            if node and node.right:
-                stack.append(node.right)
-        return res
+    def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        self.finder(root,None,None)
+        return self.res
