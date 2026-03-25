545ab055f099134f7d77b98005cb47a5022a9fc0
JoeOnTheMission <joeonthemission@gmail.com>
2026-03-25 17:55:24 +0300

    Add solution for Combinations

---
+++ leetcode/0077-combinations/0077-combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def back(start,comb):
+            #print(start,comb)
+            if len(comb) == k:
+                res.append(comb[:])
+                return 
+            for i in range(start,n+1):
+                comb.append(i)
+                back(i+1,comb)
+                comb.pop()
+        back(1,[])
+        return res
