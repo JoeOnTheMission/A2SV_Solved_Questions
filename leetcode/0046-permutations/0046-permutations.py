06e6fd950c0e2a16f0c220f1daba7928ab5aee5b
JoeOnTheMission <joeonthemission@gmail.com>
2026-03-26 23:07:56 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -2,14 +2,14 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         n = len(nums)
-        def back(start,comb):
+        def back(comb):
             if len(comb) == n:
                 res.append(comb[:])
                 return
             for i in range(0,n):
                 if nums[i] not in comb:
                     comb.append(nums[i])
-                    back(i+1,comb)
+                    back(comb)
                     comb.pop()
-        back(1,[])
+        back([])
         return res
