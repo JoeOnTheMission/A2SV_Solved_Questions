d90393bdfcf03c78532f4754eb1b16b48d513a2d
JoeOnTheMission <joeonthemission@gmail.com>
2026-04-22 17:17:58 +0300

    Add solution for Number of Pairs Satisfying Inequality

--- leetcode/2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ leetcode/2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,27 +1,26 @@
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        arr = [nums1[i] - nums2[i] for i in range(len(nums1))]
         self.res = 0
+        arr = [nums1[k] - nums2[k] for k in range(len(nums1))]
 
-        def merge_sort(l, r):
-            if l == r:
-                return [arr[l]]
+        def mergesort(left,right):
+            if left == right:
+                return [arr[left]]
 
-            mid = (l + r) // 2
-            left = merge_sort(l, mid)
-            right = merge_sort(mid + 1, r)
+            mid = left + (right - left)//2
+            left = mergesort(left,mid)
+            right = mergesort(mid+1,right)
 
-           
+            #update our res
             j = 0
             for i in range(len(left)):
                 while j < len(right) and left[i] > right[j] + diff:
                     j += 1
                 self.res += len(right) - j
-
-          
+            #merge left and right
             merged = []
-            i = j = 0
-
+            i = 0
+            j = 0
             while i < len(left) and j < len(right):
                 if left[i] <= right[j]:
                     merged.append(left[i])
@@ -29,11 +28,9 @@ class Solution:
                 else:
                     merged.append(right[j])
                     j += 1
-
             merged.extend(left[i:])
             merged.extend(right[j:])
 
             return merged
-
-        merge_sort(0, len(arr) - 1)
+        mergesort(0,len(arr)-1)
         return self.res
